0406e0f8a626dade32568ba30eda3cd04876e9c8
Jirung-E <jh10590326@gmail.com>
2023-02-12 18:48:07 +0900

    ...

--- Math.Random.cpp
+++ Math.Random.cpp
@@ -10,12 +10,12 @@ namespace Math {
     std::mt19937 gen { rd() };
 
     double random(double start, double end) {
-        static std::uniform_real_distribution<> urd { start, end };
+        std::uniform_real_distribution<> urd { start, end };
         return urd(gen);
     }
 
     int randomInt(int start, int end) {
-        static std::uniform_int_distribution<> uid { start, end };
+        std::uniform_int_distribution<> uid { start, end };
         return uid(gen);
     }
 }
