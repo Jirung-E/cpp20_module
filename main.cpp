762fe67be33a42f5f2975a025766e7a36d462169
Jirung-E <jh10590326@gmail.com>
2023-02-18 15:37:41 +0900

    circle

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@ import Math.Space;
 import IO;
 import Graphic;
 import <chrono>;
+import <numbers>;
+import <cmath>;
 
-using namespace std::chrono;
+using namespace std;
+using namespace chrono;
 using namespace Math;
 using namespace IO;
 using namespace Graphic;
@@ -11,10 +14,12 @@ using namespace Graphic;
 
 int main() {
     ConsoleView view { 50, 20 };
-    Frame frame { "frame1" };
+    Frame frame { 50, 20 };
+    Frame frame_view { 50, 20 };
     view.insert(frame);
 
-    auto entry_time = system_clock::now();
+    auto o = system_clock::now();
+    auto entry_time = o;
     while(true) {
         if(Keyboard::detected()) {
             int ch = Keyboard::get();
@@ -23,13 +28,28 @@ int main() {
             }
         }
         auto now = system_clock::now();
+        auto diff = (now - entry_time).count();
         size_t frame_per_sec = 60;
-        if(now-entry_time >= milliseconds(1000/frame_per_sec)) {
+        if(diff >= 1000/frame_per_sec) {
+            auto sec = (now - o).count() / 10'000;      // -> milliseconds
+            double rps = 2;                             // round per sec
+            int x = 15 + round(10 * sin(rps * (sec / 1'000.0) * 2*numbers::pi));
+            int y = 10 + round(5 * cos(rps * (sec / 1'000.0) * 2*numbers::pi));
+            frame_view.set(x, y, "F:F:F");
+            frame.set(x, y, "F:F:F");
+            view.insert(frame_view);
+            frame_view.set(x, y, "0:0:0");
             clear();
+            println(to_string(sec));
             view.show();
-            entry_time = system_clock::now();
+            entry_time = now;
         }
     }
+
+    frame.saveAs("circle");
+    view.insert(frame);
+    clear();
+    view.show();
 
     println("Bye~");
 }
